b51282470226ded6a95b80372f92f4a0daec8ca4
Alexander Storchak <alexander.s.storchak@gmail.com>
2019-09-11 11:14:52 +0300

    Added .idea to .gitignore. Added part of the solution for Reusable/Functions.

---
+++ Reusable/Functions.js
@@ -0,0 +1,10 @@
+'use strict';
+
+const average = (a, b) => (a + b) / 2;
+
+const resAvg = average(3, 4);
+console.log(resAvg, typeof resAvg);
+
+const square = x => x ** 2;
+console.log(square(3));
+
