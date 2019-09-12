b657213b5a7c935cc903a137d63172c2b57670c8
Alexander Storchak <alexander.s.storchak@gmail.com>
2019-09-12 22:59:12 +0300

    Added solution for Reusable/Hash.

---
+++ Reusable/Hashes.js
@@ -0,0 +1,11 @@
+'use strict';
+
+const phoneBook = {
+    'Marcus Aurelius': '+380445554433',
+    'Angela Merkel': '+380445554400',
+};
+
+const findPhoneByName = name => (phoneBook[name]) ? phoneBook[name] : 'Contact is not found.';
+
+console.log(findPhoneByName('Marcus Aurelius'));
+console.log(findPhoneByName('Donald Trump'));
