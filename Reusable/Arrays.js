4301527a8ca782a435caffe0aba190cac507d927
Alexander Storchak <alexander.s.storchak@gmail.com>
2019-09-12 11:26:32 +0300

    Added solution for Reusable/Arrays.

---
+++ Reusable/Arrays.js
@@ -0,0 +1,19 @@
+'use strict';
+
+const phoneBook = [
+    { name: 'Marcus Aurelius', phone: '+380445554433' },
+    { name: 'Angela Merkel', phone: '+380445554400' },
+];
+
+const findPhoneByName = name => {
+    let phone = 'Contact not found';
+    for (const contact of phoneBook) {
+        if (contact.name === name) {
+            phone = contact.phone;
+        }
+    }
+    return phone;
+};
+
+console.log(findPhoneByName('Marcus Aurelius'));
+console.log(findPhoneByName('Donald Trump'));
